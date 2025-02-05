cc8122ab18525475885027d5a895676686e9502a
tanya-christian <tanyachristian19@gmail.com>
2025-02-05 19:12:22 +0530

    Create welcome.blade.php

---
+++ Student/welcome.blade.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+    <head>  
+        <title>Student Registration</title>
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    
+    </head>
+<body>
+    <header>
+        <nav>
+            <a href="{{url('/'}}">        
+        </nav>
+    </header>
+    <main>
+    @yield('content')
+    </main>
+    <footer>
+        <p> &copy; 2025 Student Information </p>
+    </footer>
+</body>
+</html>
