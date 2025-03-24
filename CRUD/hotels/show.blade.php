af641aeb22e19e250a2e3bd1e60459f6b269d7e2
tanya-christian <tanyachristian19@gmail.com>
2025-03-24 21:17:20 +0530

    Delete CRUD/hotels directory

--- CRUD/hotels/show.blade.php
+++
@@ -1,6 +0,0 @@
-<h2>{{$hotel->name}}</h2>
-<p><strong>Check In Date:</strong>{{$hotel->chechindate}}</p>
-<p><strong>Check Out Date:</strong>{{$hotel->checkoutdate}}</p>
-<p><strong>Room Type:</strong>{{$hotel->room}}</p>
-
-<a href="{{route('hotels.index')}}"> Go Back</a>
